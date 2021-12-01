7e9916c8883e7940f0fca1dc23f66118110e4cb6
Anthony White <AnthonyRichardWhite@gmail.com>
2021-12-01 11:07:39 +0000

    second commit

---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+
+exports.addMovie = async (collection, dataObj) =>{
+    try{        
+        await collection.insertOne(dataObj)
+    } catch(error){
+        console.log(error)
+    }
+}
+
+exports.listMovies = async (collection) => {
+    try {
+        const listAll = await collection.find().toArray();
+        console.log(listAll);
+    } catch (error){
+        console.log(error);
+    }
+}
